f26482c808326558de66a3cd4a9d2b8605a91b3b
mnmandahalf <manaminakamura812@gmail.com>
2024-04-28 17:55:29 +0900

    added telescope keymap

--- init.lua
+++ init.lua
@@ -13,6 +13,7 @@ vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup("plugins")
 
+vim.g.mapleader = " "
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
@@ -24,4 +25,10 @@ require("nvim-tree").setup()
 
 vim.keymap.set("n", "<C-t>", "<Cmd>NvimTreeToggle<CR>")
 vim.keymap.set("n", "<C-j>", "<Cmd>NvimTreeFindFile<CR>")
+
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
 
