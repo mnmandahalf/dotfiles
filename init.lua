0474221519434c194f0015710a6eae3788797b0f
mnmandahalf <manaminakamura812@gmail.com>
2024-05-06 17:28:55 +0900

    refactor telescope setting

--- init.lua
+++ init.lua
@@ -9,9 +9,6 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
     lazypath,
   })
 end
-vim.opt.rtp:prepend(lazypath)
-
-require("lazy").setup("plugins")
 
 vim.g.mapleader = " "
 vim.g.loaded_netrw = 1
@@ -19,16 +16,12 @@ vim.g.loaded_netrwPlugin = 1
 
 vim.opt.number = true
 
+vim.opt.rtp:prepend(lazypath)
+require("lazy").setup("plugins")
 vim.cmd 'colorscheme material'
 
 require("nvim-tree").setup()
 
 vim.keymap.set("n", "<C-t>", "<Cmd>NvimTreeToggle<CR>")
 vim.keymap.set("n", "<C-j>", "<Cmd>NvimTreeFindFile<CR>")
-
-local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
-vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
 
